34c62880ff1bf9d002aeabf9070c55db9eace980
reidys <reidys@student.ethz.ch>
2022-10-20 22:47:53 +0200

    redo q5 of debugging

--- lectures/Testing/exercises/Q5/src/test/java/Vector3Test.java
+++ lectures/Testing/exercises/Q5/src/test/java/Vector3Test.java
@@ -6,50 +6,96 @@ final class Vector3Test {
 
     // should also write a test for the constructor!
     @Test
-    public void IsZeroVectorTrue() {
-        Vector3 vec = new Vector3(0,0,0);
-        assertThat(vec.isZero(), is(true));
+    public void ConstructorInt() {
+        Vector3 test = new Vector3(1,2,3);
+        assertThat(test.getX(), is(1.0));
+        assertThat(test.getY(), is(2.0));
+        assertThat(test.getZ(), is(3.0));
     }
 
     @Test
-    public void IsZeroVectorFalse() {
-        Vector3 vec = new Vector3(-8.66,1.2,234.66);
-        assertThat(vec.isZero(), is(false));
+    public void ConstructorDouble() {
+        Vector3 test = new Vector3(1.0,2.0,3.0);
+        assertThat(test.getX(), is(1.0));
+        assertThat(test.getY(), is(2.0));
+        assertThat(test.getZ(), is(3.0));
     }
 
     @Test
-    public void ComputeDotProduct5() {
-        Vector3 vec = new Vector3(1,2,3);
-        Vector3 vec2 = new Vector3(2,3,-1);
-        assertThat(vec.dotProduct(vec2), is(5.0));
+    public void ConstructorZero() {
+        Vector3 test = new Vector3();
+        assertThat(test.getY(), is(0.0));
+        assertThat(test.getZ(), is(0.0));
+        assertThat(test.getX(), is(0.0));
     }
 
     @Test
-    public void ComputeLength5() {
-        Vector3 vec = new Vector3(0,-4,3);
-        assertThat(vec.computeLength(), is(5.0));
+    public void IsZeroTrue() {
+        Vector3 test = new Vector3();
+        assertThat(test.isZero(), is(true));
     }
 
     @Test
-    public void NormalizeVector5() {
-        Vector3 vec = new Vector3(0,4,-3);
-        vec.normalize();
-        assertThat(vec.toString(), is("[0.0, 0.8, -0.6]"));
+    public void IsZeroFalse() {
+        Vector3 test = new Vector3(0,0,1);
+        assertThat(test.isZero(), is(false));
     }
 
     @Test
-    public void SclaeByScalar5() {
-        Vector3 vec = new Vector3(0,4,-3);
-        vec.scale(4);
-        assertThat(vec.toString(), is("[0.0, 16.0, -12.0]"));
+    public void DotProduct() {
+        Vector3 test = new Vector3(1,2,3);
+        Vector3 testa = new Vector3(3,2,1);
+        assertThat(test.dotProduct(testa), is(10.0));
     }
 
     @Test
-    public void ComputeCrossProduct() {
-        Vector3 vec = new Vector3(1,2,3);
-        Vector3 vec2 = new Vector3(4,5,6);
-        Vector3 result = vec.crossProduct(vec2);
-        assertThat(result.toString(), is("[-3.0, 6.0, -3.0]"));
+    public void ComputeLength() {
+        Vector3 test = new Vector3(4,3,0);
+        assertThat(test.computeLength(), is(5.0));
     }
+
+    @Test
+    public void Normalize() {
+        Vector3 test = new Vector3(4,3,0);
+        test.normalize();
+        assertThat(test.getX(), is( 4.0 /5.0));
+        assertThat(test.getY(), is(3.0 / 5.0));
+    }
+
+    @Test
+    public void Scale() {
+        Vector3 test = new Vector3(4,3,0);
+        test.scale(5);
+        assertThat(test.getX(), is( 4.0 *5.0));
+        assertThat(test.getY(), is(3.0 * 5.0));
+    }
+
+    @Test
+    public void CrossProduct() {
+        Vector3 test = new Vector3(1,0,0);
+        Vector3 testa = new Vector3(0,0,1);
+        test = test.crossProduct(testa);
+        assertThat(test.getX(), is( 0.0));
+        assertThat(test.getY(), is(-1.0));
+        assertThat(test.getZ(), is(0.0));
+    }
+
+    @Test
+    public void ToString() {
+        Vector3 test = new Vector3(1,0,0);
+        assertThat(test.toString(), is("[1.0, 0.0, 0.0]"));
+    }
+
+    @Test
+    public void Setters() {
+        Vector3 test = new Vector3();
+        test.setX(1);
+        test.setY(1);
+        test.setZ(1);
+        assertThat(test.getX(), is(1.0));
+        assertThat(test.getY(), is(1.0));
+        assertThat(test.getZ(), is(1.0));
+    }
+
 
 }
